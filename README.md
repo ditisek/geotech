6bec3706d9cfa3e4b1a48a1b0518de65adef81b1
ditisek <ditisek@gmail.com>
2021-03-12 12:54:01 +0200

    Folder Structure Correction

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # geotech
 Data import, conversion and saving modules for Geotech data from airborne data acquisition sytem
-GIT Push 11:18
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='VTEMExtract',
+    name='geotech',
     version='0.1',
     packages=[''],
     url='',
